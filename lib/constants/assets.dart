e573bdf5f1d469f6c6bde7f88af4fb9f704bd3b0
mr-riad <riadgangni01302@gmail.com>
2025-07-01 15:55:37 +0600

    add assets url and responsive dimention

---
+++ lib/constants/assets.dart
@@ -0,0 +1,10 @@
+class Assets {
+  static const String profileImage = "assets/images/";
+  static const String project1 = "assets/images/";
+  static const String project2 = "assets/images/";
+  static const String project3 = "assets/images/";
+  static const String github = "assets/icons/";
+  static const String linkedin = "assets/icons/";
+  static const String twitter = "assets/icons/";
+  static const String email = "assets/icons/";
+}
---
+++ lib/constants/dimensions.dart
@@ -0,0 +1,28 @@
+class Dimensions {
+  // Padding and margin
+  static const double paddingExtraSmall = 4.0;
+  static const double paddingSmall = 8.0;
+  static const double paddingMedium = 16.0;
+  static const double paddingLarge = 24.0;
+  static const double paddingExtraLarge = 32.0;
+
+  // Font sizes
+  static const double fontSizeSmall = 12.0;
+  static const double fontSizeMedium = 14.0;
+  static const double fontSizeLarge = 16.0;
+  static const double fontSizeExtraLarge = 20.0;
+  static const double fontSizeHeading = 24.0;
+  static const double fontSizeTitle = 32.0;
+  static const double fontSizeBigTitle = 48.0;
+
+  // Icon sizes
+  static const double iconSizeSmall = 16.0;
+  static const double iconSizeMedium = 24.0;
+  static const double iconSizeLarge = 32.0;
+  static const double iconSizeExtraLarge = 48.0;
+
+  // Button sizes
+  static const double buttonHeight = 48.0;
+  static const double buttonRadius = 8.0;
+  static const double buttonBorderWidth = 2.0;
+}
