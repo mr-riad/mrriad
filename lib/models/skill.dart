d1cfc81cd1ba40690ccdd3ab47addde407f6b243
mr-riad <riadgangni01302@gmail.com>
2025-07-01 15:59:38 +0600

    model add

---
+++ lib/models/skill.dart
@@ -0,0 +1,11 @@
+class Skill {
+  final String name;
+  final int proficiency; // Out of 100
+  final String iconPath;
+
+  Skill({
+    required this.name,
+    required this.proficiency,
+    required this.iconPath,
+  });
+}
