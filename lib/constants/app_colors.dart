b3292a7011dae2c540b270c7451cfb689a8ae500
mr-riad <riadgangni01302@gmail.com>
2025-07-01 15:50:51 +0600

    add appText and appColor

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF6C63FF);
+  static const Color secondaryColor = Color(0xFF3F3D56);
+  static const Color backgroundColor = Color(0xFFF9F9F9);
+  static const Color textColor = Color(0xFF2D3748);
+  static const Color lightTextColor = Color(0xFF718096);
+  static const Color whiteColor = Colors.white;
+  static const Color blackColor = Colors.black;
+  static const Color successColor = Color(0xFF48BB78);
+  static const Color errorColor = Color(0xFFE53E3E);
+}
---
+++ lib/constants/app_text.dart
@@ -0,0 +1,15 @@
+class AppText {
+  static const String homeTitle = "Hi, I'm John Doe";
+  static const String homeSubtitle = "Flutter Developer & Digital Marketer";
+  static const String aboutTitle = "About Me";
+  static const String aboutContent =
+      "I'm a passionate Flutter developer with 3 month of experience building beautiful and performant cross-platform applications. I love solving complex problems and creating intuitive user experiences.";
+  static const String projectsTitle = "My Projects";
+  static const String contactTitle = "Get In Touch";
+  static const String contactSubtitle =
+      "Feel free to reach out for collaborations or just a friendly hello";
+  static const String contactFormTitle = "Send Me a Message";
+  static const String hireMe = "Hire Me";
+  static const String viewProjects = "View Projects";
+  static const String downloadCV = "Download CV";
+}
